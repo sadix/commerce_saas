3911d96d248b62ba0ee28dbb07bbc1c4f58c92f6
Sadix <tineserignesaliou@gmail.com>
2026-09-12 17:00:58 +0000

    Switch database pooler to transactionnal & directUrl for deployements

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,6 +12,7 @@ const globalForPrisma = globalThis as unknown as {
  const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
   shadowDatabaseConnectionString: process.env.SHADOW_DATABASE_URL!,
+  directUrl: process.env.DIRECT_URL!,
 });
 
 //export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter});
